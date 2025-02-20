2a4e69e8dd427fe0d569310b6351a6a463cf5ea2
muhammad-ali62 <ali62hafiz@gmail.com>
2025-02-20 19:18:41 +0500

    Update scripct.js

--- scripct.js
+++ scripct.js
@@ -6,19 +6,23 @@ async function checkWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`;
     const weather_data = await fetch(url).then(response => response.json());
 console.log(weather_data);
-    // Manually assign data to each day
+if(weather_data.cod === `404`){
+    location_not_found.style.display = "flex";
+     weather_body.style.display = "none";
+    console.log("error");
+    return;
+}
+    
     for (let i = 0; i < 5; i++) {
         const day = weather_data.list[i];
 
-        // Set temperature and humidity for each day
-        document.getElementById(`temp${i + 1}`).textContent = `${Math.round(day.main.temp - 273.15)}°C`; // Convert from Kelvin to Celsius
+        document.getElementById(`temp${i + 1}`).textContent = `${Math.round(day.main.temp - 273.15)}°C`; 
+
         document.getElementById(`humidity${i + 1}`).textContent = `${day.main.humidity}%`;
 
-        // Set description for each day
-        document.getElementById(`desp${i + 1}`).textContent = day.weather[0].description; // Weather description
+        document.getElementById(`desp${i + 1}`).textContent = day.weather[0].description; 
     }
 }
-
 searchBtn.addEventListener('click', () => {
     checkWeather(inputBox.value);
 });
